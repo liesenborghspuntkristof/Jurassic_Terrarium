9e5cc43068b322d1b8ba3e50f47597ec0b389725
annemieps <aroelants@gmail.com>
2016-01-13 16:02:16 +0100

    Scrum dag 1
    Home pagina voor user en admin control panel

---
+++ admin.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Jurrasic Terrarium Home</title>
+         <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href='https://fonts.googleapis.com/css?family=Roboto:100,400' rel='stylesheet' type='text/css'>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    
+    </head>
+    <body>
+        
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="js/bootstrap.min.js"></script>
+                
+            <header>
+                Logged in as Admin   <a href="#">  <span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>  logout</a>
+            </form>
+        </header>    
+            <div class="container">
+                <h1>Admin Panel</h1>
+               <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Kies user
+                <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                  <li>User 1</li>
+                  <li>User 2</li>
+                  <li>User 3</li>
+                </ul>
+              </div>
+                <br>
+                
+                <table class="table col-md-6">
+                    <tbody>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    </tbody>
+                </table>
+                <table class="table col-md-6">
+                    <tbody>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    <tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+                    </tbody>
+                </table>
+                <section id="TerrariumNav" class="col-md-6">
+                    <p><button type="button" class="btn btn-default" aria-label="Left Align">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"> </span><br>Previous
+                    </button>
+                    <button type="button" class="btn btn-default" aria-label="Left Align">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><br>Next</button></p> 
+                    <br>
+                    
+                </section>
+                
+            </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
